3b83625b1d8ee0704655141cf50984bb925a43a4
Sharon Shalem <shrnshlm@gmail.com>
2023-09-20 13:37:04 +0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')  
 def upload():  
-    return render_template("file_upload_form.html")  
+    return render_template("index.html")  
 
 @app.route('/success', methods = ['POST'])  
 def success():  
